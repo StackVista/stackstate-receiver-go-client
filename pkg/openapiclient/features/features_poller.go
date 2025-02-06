e10cda1201abc5360a45271e1ef994dbc1120a90
Bram Schuur <bschuur@stackstate.com>
2025-02-06 11:20:40 +0100

    STAC-22339: Add feature poller

---
+++ pkg/openapiclient/features/features_poller.go
@@ -0,0 +1,42 @@
+package features
+
+import (
+	"context"
+	"github.com/StackVista/stackstate-receiver-go-client/generated/receiver_api"
+	log "github.com/cihub/seelog"
+	"net/http"
+	"time"
+)
+
+func StartFeaturesPoller(clientCtx context.Context, featuresApi receiver_api.FeaturesAPI, interval time.Duration) (chan map[string]interface{}, func()) {
+
+	outputChannel := make(chan map[string]interface{})
+	stopChannel := make(chan interface{})
+	ticker := time.NewTicker(interval)
+
+	go func() {
+		for {
+			select {
+			case <-stopChannel:
+				ticker.Stop()
+				close(outputChannel)
+				return
+			case <-ticker.C:
+				features, response, err := featuresApi.GetFeaturesExecute(featuresApi.GetFeatures(clientCtx))
+				if err != nil {
+					log.Errorf("Error retrieving features: %v", err)
+					continue
+				}
+
+				if response.StatusCode != http.StatusOK {
+					log.Errorf("Error retrieving features, statuscode was: %s", response.Status)
+					continue
+				}
+
+				outputChannel <- features
+			}
+		}
+	}()
+
+	return outputChannel, func() { close(stopChannel) }
+}
